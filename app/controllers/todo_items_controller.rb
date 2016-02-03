97636fa0b05a494f23baf4aaf4a4c655489a85df
Tucker Mullen <mullentucker@gmail.com>
2016-02-03 10:12:32 -0800

    Mark as complete ability

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,5 +1,6 @@
 class TodoItemsController < ApplicationController
   before_action :set_todo_list
+  before_action :set_todo_item, except: [:create]
 
   def create
     @todo_item = @todo_list.todo_items.create(todo_item_params)
@@ -8,7 +9,6 @@ class TodoItemsController < ApplicationController
   end
 
   def destroy
-    @todo_item = @todo_list.todo_items.find(params[:id])
     if @todo_item.destroy
       flash[:success] = "Todo List item was deleted!"
     else
@@ -16,6 +16,11 @@ class TodoItemsController < ApplicationController
     end
 
     redirect_to @todo_list
+  end
+
+  def complete
+    @todo_item.update_attribute(:completed_at, Time.now)
+    redirect_to @todo_list, notice: "Todo Item Complete!"
   end
 
   private
@@ -26,5 +31,9 @@ class TodoItemsController < ApplicationController
 
   def todo_item_params
     params[:todo_item].permit(:content)
+  end
+
+  def set_todo_item
+    @todo_item = @todo_list.todo_items.find(params[:id])
   end
 end
